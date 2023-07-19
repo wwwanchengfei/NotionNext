cc278a53d45aa939a3ad8eaccbb4967ebba59080
wan.chengfei <dev.wanchengfei@gmail.com>
2023-07-19 11:49:52 +0800

    no message

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -15,11 +15,11 @@ export default function TouchMeCard() {
                 frontContent={
                     <div className='h-full'>
                         <h2 className='font-[1000] text-3xl'>交流频道</h2>
-                        <h3 className='pt-2'>加入我们的社群讨论分享</h3>
+                        <h3 className='pt-2'>加入社群讨论分享</h3>
                         <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat' }}></div>
                     </div>}
                 backContent={<div className='font-[1000] text-xl h-full'>
-                    <Link href='https://docs.tangly1024.com/article/how-to-question'>
+                    <Link href=''>
                         点击加入社群
                     </Link>
                 </div>}
