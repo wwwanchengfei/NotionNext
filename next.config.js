d73273cc455b3e56ca510e67e99d6f165f7d8b38
wan.chengfei <dev.wanchengfei@gmail.com>
2023-08-08 17:17:09 +0800

    add eslint config

--- next.config.js
+++ next.config.js
@@ -107,5 +107,9 @@ module.exports = withBundleAnalyzer({
   publicRuntimeConfig: { // 这里的配置既可以服务端获取到，也可以在浏览器端获取到
     NODE_ENV_API: process.env.NODE_ENV_API || 'prod',
     THEMES: themes
-  }
+  },
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+},
 })
