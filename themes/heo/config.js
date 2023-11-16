45be136577618242f5d9c38d5e94de4882535fa7
wan.chengfei <wan.chengfei@rongzer.com>
2023-11-16 15:42:47 +0800

    配置信息

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,21 +1,21 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2022-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: '' },
-    { title: '访问文档中心获取更多帮助', url: '' }
+    { title: '欢迎来到我的博客', url: 'https://www.wanchengfei.com' },
+    { title: '分享简单生活', url: 'https://www.wanchengfei.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享生活',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: '',
+  HEO_HERO_TITLE_3: 'WANCHENGFEI.COM',
+  HEO_HERO_TITLE_4: '少愤慨，多谦卑',
+  HEO_HERO_TITLE_5: '慎言、慎行、慎独',
+  HEO_HERO_TITLE_LINK: 'https://www.wanchengfei.com',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -37,7 +37,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: '', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://www.wanchengfei.com', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
