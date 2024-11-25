267462157a3360ad2e19fd334c3592a633720ecc
wan.chengfei <wan.chengfei@rongzer.com>
2024-11-25 13:21:12 +0800

    no message

--- pages/memos/index.js
+++ pages/memos/index.js
@@ -1,14 +1,12 @@
-import { useRouter } from 'next/router'
-import { getLayoutByTheme } from '@/themes/theme'
 import { siteConfig } from '@/lib/config'
 import { getGlobalData } from '@/lib/notion/getNotionData'  //若为NotionNext 4.3.2+版本，此处应为 @/lib/db/getSiteData
 import React from 'react'
 import BLOG from '@/blog.config'
+import { DynamicLayout } from '@/themes/theme'
 
 const MemosIndex = props => {
-  const Layout = getLayoutByTheme({ theme: siteConfig('THEME'), router: useRouter() })
-
-  return <Layout {...props} />
+  const theme = siteConfig('THEME', BLOG.THEME, props.NOTION_CONFIG)
+  return <DynamicLayout theme={theme} layoutName='LayoutMemosIndex' {...props} />
 }
 
 export async function getStaticProps() {
